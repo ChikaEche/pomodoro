77211b69bc7bc119645f4e0b394b1fa2f53a1f7c
William Akinsanya <akinsanya.will@gmail.com>
2020-05-10 12:04:36 +0100

    spec out timer class

---
+++ src/app/shared/timer.ts
@@ -0,0 +1,18 @@
+export class Timer {
+  private seconds: number;
+
+  // Add more instace variables / methods as you see fit, just keep the code clean
+
+  constructor(readonly secs: number) {
+    this.seconds = secs;
+  }
+
+  // TODO
+  start() {}
+
+  // TODO
+  pause() {}
+
+  // TODO
+  getDisplayTime() {}
+}
