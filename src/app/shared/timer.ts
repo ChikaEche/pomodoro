a8d4cd7e9deefacba505dc94826866363603893f
William Akinsanya <akinsanya.will@gmail.com>
2020-05-10 12:07:06 +0100

    flesh out comment in timer class

--- src/app/shared/timer.ts
+++ src/app/shared/timer.ts
@@ -2,6 +2,7 @@ export class Timer {
   private seconds: number;
 
   // Add more instace variables / methods as you see fit, just keep the code clean
+  // expose observables for key event such as time end / pause
 
   constructor(readonly secs: number) {
     this.seconds = secs;
