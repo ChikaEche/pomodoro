09be327301da0c5b982800e54a8062c0817c3dac
William Akinsanya <akinsanya.will@gmail.com>
2020-06-04 12:56:10 -0400

    migrate layout

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-import { BreakpointService } from 'src/app/core/services/breakpoint.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointService } from '../core/services/breakpoint.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent {
-  constructor(public readonly breakpointService: BreakpointService) {}
+export class LayoutComponent implements OnInit {
+  @ViewChild('sidenav') sidenav: MatSidenav;
+
+  breakPoint$: Observable<boolean>;
+
+  constructor(private breakPointService: BreakpointService) {
+    this.breakPoint$ = this.breakPointService.isPalm$;
+  }
+
+  ngOnInit(): void {}
 }
