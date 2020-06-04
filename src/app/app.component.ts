cbd07e5ceaa6b85c6eb7a810bc9f0c2fc4f2568e
William Akinsanya <akinsanya.will@gmail.com>
2020-06-04 13:02:13 -0400

    fade animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {
+  trigger,
+  transition,
+  query,
+  style,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition('* <=> *', [
+        query(
+          ':enter, :leave',
+          [
+            style({
+              position: 'absolute',
+              left: 0,
+              width: '100%',
+              opacity: 0,
+              transform: 'scale(0) translateY(100%)',
+            }),
+          ],
+          { optional: true }
+        ),
+        query(
+          ':enter',
+          [
+            animate(
+              '500ms ease',
+              style({ opacity: 1, transform: 'scale(1) translateY(0)' })
+            ),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   constructor(private readonly router: Router) {}
