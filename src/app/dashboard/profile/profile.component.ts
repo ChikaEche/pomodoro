0aac78a830b7200f5c81b4035656dd34b1df3008
Chika <chikaokafor94@gmail.com>
2020-06-09 11:26:57 +0100

    profile design

--- src/app/dashboard/profile/profile.component.ts
+++ src/app/dashboard/profile/profile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { tap } from 'rxjs/operators';
+import { User } from 'src/app/shared/interfaces/user.interface';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,7 +12,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  constructor() {}
+  userProfile: User;
+  profileState = 'manage-account';
 
-  ngOnInit(): void {}
+  password = new FormGroup({
+    currentPassword: new FormControl('', [Validators.required]),
+    newPassword: new FormControl('', [Validators.required]),
+    retypePassword: new FormControl('', [Validators.required]),
+  });
+
+  profile = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+  });
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.authService.user$
+      .pipe(
+        tap((user) => {
+          this.userProfile = user;
+          this.profile.setValue({
+            name: this.userProfile.displayName,
+            email: this.userProfile.email,
+          });
+        })
+      )
+      .subscribe({ error: (err) => console.warn('error occured') });
+  }
+
+  profileStateToggle(state: string) {
+    if (state === 'manage-account') {
+      this.profileState = 'manage-account';
+    } else if (state === 'password-change') {
+      this.profileState = 'password-change';
+    }
+  }
 }
